38ea70c48c45ad335f4128a23d3eea41538a707a
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-06-26 01:12:08 +0530

    added controller test cases

--- src/controllers/candidate.ts
+++ src/controllers/candidate.ts
@@ -91,47 +91,47 @@ export const addCandidate = async (
   }
 };
 
-export const updateCandidate = async (
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  logger.info("Adding candidate for the user");
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new CustomError(getCustomErrorMessage(VALIDATION_FAILED, errors), 422)
-    );
-  }
-  const {
-    name,
-    email,
-    dob,
-    phone,
-    location,
-    zipcode,
-    socialSecurity,
-    driverLicence,
-    recruiterId,
-  } = req.body;
-  try {
-    const candidate = new Candidate({
-      name: name,
-      email: email,
-      dob: dob,
-      phone: phone,
-      location: location,
-      zipcode: zipcode,
-      socialSecurity: socialSecurity,
-      driverLicence: driverLicence,
-      createdAt: new Date(),
-      recruiterId: recruiterId,
-    });
-    const result = await candidate.save();
-    res
-      .status(201)
-      .json({ message: CANDIDATE_CREATED, candidateId: result._id });
-  } catch (error) {
-    next(error);
-  }
-};
+// export const updateCandidate = async (
+//   req: CustomRequest,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   logger.info("Adding candidate for the user");
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return next(
+//       new CustomError(getCustomErrorMessage(VALIDATION_FAILED, errors), 422)
+//     );
+//   }
+//   const {
+//     name,
+//     email,
+//     dob,
+//     phone,
+//     location,
+//     zipcode,
+//     socialSecurity,
+//     driverLicence,
+//     recruiterId,
+//   } = req.body;
+//   try {
+//     const candidate = new Candidate({
+//       name: name,
+//       email: email,
+//       dob: dob,
+//       phone: phone,
+//       location: location,
+//       zipcode: zipcode,
+//       socialSecurity: socialSecurity,
+//       driverLicence: driverLicence,
+//       createdAt: new Date(),
+//       recruiterId: recruiterId,
+//     });
+//     const result = await candidate.save();
+//     res
+//       .status(201)
+//       .json({ message: CANDIDATE_CREATED, candidateId: result._id });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
